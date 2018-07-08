beb511f555ed1a9cd1aea23d61673a0ebded4470
Surapong.suw <surapong.suwa@gmail.com>
2018-07-08 15:31:44 +0700

    implement saga

--- actions.js
+++ actions.js
@@ -1,36 +1,9 @@
 import { 
-  FETCHING_DATA, 
-  FETCHING_DATA_SUCCESS, 
-  FETCHING_DATA_FAILURE 
+  FETCHING_DATA
 } from './constants'
-import getPeople from './api'
-
-export function getData() {
-  return {
-    type: FETCHING_DATA
-  }
-}
-
-export function getDataSuccess(data) {
-  return {
-    type: FETCHING_DATA_SUCCESS,
-    data,
-  }
-}
-
-export function getDataFailure() {
-  return {
-    type: FETCHING_DATA_FAILURE
-  }
-}
 
 export function fetchData() {
-  return (dispatch) => {
-    dispatch(getData())
-    getPeople()
-      .then((data) => {
-        dispatch(getDataSuccess(data))
-      })
-      .catch((err) => console.log('err:', err))
+  return {
+    type: FETCHING_DATA
   }
 }
--- configureStore.js
+++ configureStore.js
@@ -4,15 +4,20 @@ import {
   compose 
 } from 'redux'
 import app from './reducers'
-import thunk from 'redux-thunk'
+
+import createSagaMiddleware from 'redux-saga'
+import dataSaga from './saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 export default function configureStore() {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   let store = createStore(
     app, 
     composeEnhancers(
-      applyMiddleware(thunk)
+      applyMiddleware(sagaMiddleware)
     )
   )
+  sagaMiddleware.run(dataSaga)
   return store
 }
